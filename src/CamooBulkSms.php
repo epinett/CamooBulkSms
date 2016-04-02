82c90a2cad9eb7db9bb8ce6c1a5a1528bfc59319
Epi TNE <e.tchabom@camoo.cm>
2016-04-02 17:29:57 +0200

    Update CamooBulkSms.php

--- src/CamooBulkSms.php
+++ src/CamooBulkSms.php
@@ -201,7 +201,7 @@ class CamooBulkSms {
 
             // Find the total cost of this message
             $response_obj->cost = $total_cost = 0;
-            if (is_array($response_obj->messages)) {
+            if ( $response_obj->code == 200 && is_array($response_obj->messages) ) {
                 foreach ($response_obj->messages as $msg) {
                     if (property_exists($msg, "messageprice")) {
                         $total_cost = $total_cost + (float)$msg->messageprice;
@@ -257,7 +257,7 @@ class CamooBulkSms {
     public function displayOverview( $oResponse =null ){
         $orInfo = ( $oResponse !== null ) ? $this->camoo_response : $oResponse;
 
-        if (!$oResponse ) return 'Cannot display an overview of this response';
+        if (!$oResponse || $oResponse->code != 200) return 'Cannot display an overview of this response';
         #$info = $oResponse->sms;
         $info = $oResponse;
         // How many messages were sent?
