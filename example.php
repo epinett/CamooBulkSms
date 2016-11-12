08377d6dd7af19e7de7092fe3123d971dbc4ca70
Epi TNE <e.tchabom@camoo.cm>
2016-11-12 15:46:58 +0100

    Update example.php

--- example.php
+++ example.php
@@ -1,11 +1,11 @@
 <?php
-    include_once( "src/CamooBulkSms.php" );
+    include_once( "src/CamooSms.php" );
     /**
      * @Brief Send a sms
      *
      */
     // Step 1: Declare new CamooBulkSms.
-    $oSMS = new CamooBulkSms('api_key', 'secret_key');
+    $oSMS = new CamooSms('api_key', 'secret_key');
     // Step 2: Use sendText( $to, $from, $message ) method to send a message.
     $orSMS = $oSMS->sendText( '+237612345678', 'YourCompany', 'Hello kmer world!' );
 
