f4a41fc532eec7869da54ed6b039e7fb0fea8b9c
Epi TNE <e.tchabom@camoo.cm>
2016-11-20 13:56:34 +0100

    example sms to multi recipients added

--- examples/message_example.php
+++ examples/message_example.php
@@ -9,6 +9,9 @@
     // Step 2: Use sendText( $to, $from, $message ) method to send a message.
     $orSMS = $oSMS->sendText( '+237612345678', 'YourCompany', 'Hello kmer world!' );
 
+    // OR Send the same message to multi-recipients
+     $orSMS = $oSMS->sendText( ['+237612345678', '+237612345679', '+237612345610',], 'YourCompany', 'Hello kmer world!' );
+
 // Optional
     // Step 3: Display an overview of the message
     echo $oSMS->displayOverview($orSMS);
